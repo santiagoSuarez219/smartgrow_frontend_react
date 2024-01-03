432238e865054c721545b7bd4695c021313f69ea
santiagoSuarez219 <santiago8628@gmail.com>
2024-01-03 14:46:21 -0500

    Add SweetAlert2 and SweetAlert2-react-content dependencies

--- src/Components/ActuadoresButtons/index.jsx
+++ src/Components/ActuadoresButtons/index.jsx
@@ -1,9 +1,36 @@
 import React from "react";
 
+import Swal from "sweetalert2";
+
 const ActuadoresButtons = () => {
   return (
     <div className="px-4 pt-8 fixed left-0 bottom-16 bg-white h-52 w-full rounded-t-md shadow flex flex-col items-center gap-4">
-      <button className="w-full border-2 border-primary rounded-md p-2 text-secondary">
+      <button
+        className="w-full border-2 border-primary rounded-md p-2 text-secondary"
+        onClick={() => {
+          Swal.fire({
+            title: "¿Estas seguro?",
+            text: "Vas a activar la entrada de agua",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#6A994E",
+            cancelButtonColor: "#BC4749",
+            confirmButtonText: "Si, activar",
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                title: "¡Activando!",
+                text: "Estamos activando la entrada de agua",
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: () => {
+                  Swal.showLoading();
+                },
+              });
+            }
+          });
+        }}
+      >
         Entrada de agua
       </button>
       <button className="w-full border-2 border-primary rounded-md p-2 text-secondary">
--- src/Components/SetPointForm/index.jsx
+++ src/Components/SetPointForm/index.jsx
@@ -1,9 +1,13 @@
 import React, { useContext, useState } from "react";
 import { SmartgrowContext } from "../../SmartgrowContext";
 
-const SetPointForm = () => {
-  const { setOpenModal, valueModal } = useContext(SmartgrowContext);
+import Swal from "sweetalert2";
 
+const SetPointForm = () => {
+  const { setOpenModalControl, setSetPointPh, setPointEc } =
+    useContext(SmartgrowContext);
+
+  const [setPointLabel, setSetPointLabel] = useState("");
   const [newValue, setNewValue] = React.useState("");
 
   const onSubmit = (event) => {
@@ -15,21 +19,25 @@ const SetPointForm = () => {
   };
 
   const onCancel = () => {
-    setOpenModal(false);
+    setOpenModalControl(false);
   };
 
   return (
     <form
-      className="px-4 pt-8 fixed left-0 bottom-16 bg-white h-64 w-full rounded-t-md shadow flex flex-col items-center gap-4"
+      className="px-4 pt-4 fixed left-0 bottom-16 bg-white h-64 w-full rounded-t-md shadow flex flex-col gap-4"
       onSubmit={onSubmit}
     >
-      <div className="w-full flex gap-2">
-        <button className="w-1/2 border-2 border-primary p-2 rounded-md text-sm text-secondary">
-          PH
-        </button>
-        <button className="w-1/2 border-2 border-primary p-2 rounded-md text-sm text-secondary">
-          Electroconductividad
-        </button>
+      <div>
+        <p className="text-sm">Eliga el valor que desea modificar</p>
+        <select
+          className="w-1/2 border-2 p-1 rounded-md text-sm text-black text-center"
+          name="control_label"
+          id="control_label"
+          onChange={(e) => setSetPointLabel(e.target.value)}
+        >
+          <option value="ph">PH</option>
+          <option value="ec">Electroconductividad</option>
+        </select>
       </div>
       <div className="w-full flex flex-col">
         <p className="text-xs text-primary">El valor actual es 0.32</p>
@@ -38,42 +46,48 @@ const SetPointForm = () => {
           name="value"
           id="value"
           placeholder="Escriba el valor del SetPoint"
-          className="w-full border-2 border-primary rounded-md p-1 text-sm text-secondary"
+          onChange={onChange}
+          value={newValue}
+          className="w-full border-2 border-primary rounded-md p-1 text-sm text-black"
         />
       </div>
-      <div className="w-full flex gap-2">
-        <button className="w-1/2 border-2 border-quartiary p-2 rounded-md text-sm text-quartiary">
-          Cancelar
-        </button>
-        <button className="w-1/2 border-2 border-primary p-2 rounded-md text-sm text-secondary">
+      <div className="w-full flex justify-end">
+        <button
+          className="w-1/2 border-2 border-primary p-2 rounded-md text-sm text-secondary"
+          onClick={() => {
+            Swal.fire({
+              title: "¿Estas seguro?",
+              text: "Vas a modificar el valor del setpoint de PH",
+              icon: "warning",
+              showCancelButton: true,
+              confirmButtonColor: "#6A994E",
+              cancelButtonColor: "#BC4749",
+              confirmButtonText: "Si, modificar",
+            }).then((result) => {
+              if (result.isConfirmed) {
+                Swal.fire({
+                  title: "¡Cambiando el valor del PH!",
+                  text: "Estamos cambiando el valor del PH",
+                  timer: 3000,
+                  timerProgressBar: true,
+                  didOpen: () => {
+                    Swal.showLoading();
+                  },
+                }).then(() => {
+                  Swal.fire({
+                    title: "¡Listo!",
+                    text: "El valor del PH fue modificado",
+                    icon: "success",
+                    confirmButtonColor: "#6A994E",
+                  });
+                });
+              }
+            });
+          }}
+        >
           Enviar
         </button>
       </div>
-      {/* <label className="mb-6 text-center font-bold text-base">
-        Escribe el valor del setpoint {valueModal}
-      </label>
-      <input
-        className="w-full border-2 boorder-solid border-primary rounded-lg shadow-sm text-center "
-        type="text"
-        placeholder="0.00"
-        onChange={onChange}
-        value={newValue}
-      ></input>
-      <div className="mt-4 w-full flex justify-end space-x-2">
-        <button
-          className="w-1/2 p-2 rounded-lg shadow-sm border-2 border-red-500 hover:bg-red-400"
-          type="button"
-          onClick={onCancel}
-        >
-          Cancelar
-        </button>
-        <button
-          className="w-1/2 p-2 rounded-lg shadow-sm border-2 border-primary hover:bg-green-500"
-          type="submit"
-        >
-          Enviar
-        </button>
-      </div> */}
     </form>
   );
 };
