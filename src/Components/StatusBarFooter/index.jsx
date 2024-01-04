be44d8f79cccc3f831fb316cf66639d130db89e1
santiagoSuarez219 <santiago8628@gmail.com>
2024-01-04 09:03:35 -0500

    Update StatusBarFooter styling and content

--- src/Components/StatusBarFooter/index.jsx
+++ src/Components/StatusBarFooter/index.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const StatusBarFooter = () => {
   return (
-    <div className="w-full px-4 fixed bottom-20 z-20">
-      <div className="bg-slate-400 px-2 py-0.5 rounded-md flex justify-between">
-        <p className="text-base font-semibold">
+    <div className="w-full px-4 fixed bottom-20 z-20 lg:bottom-6">
+      <div className="bg-slate-400 px-2 py-0.5 rounded-md font-semibold flex text-base justify-between lg:text-2xl lg:px-6">
+        <p>
           16 <span className="font-normal">Recirculaciones</span>
         </p>
         <p>03 Enero 2024</p>
