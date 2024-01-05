19da6f6c2fd8d5625895c586276dab29ede9571c
santiagoSuarez219 <santiago8628@gmail.com>
2024-01-05 16:37:35 -0500

    Add temperature graph and time range selection

--- src/Components/GraficaForm/index.jsx
+++ src/Components/GraficaForm/index.jsx
@@ -1,7 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import ReactApexChart from "react-apexcharts";
+import { SmartgrowContext } from "../../SmartgrowContext";
+
+import { HiOutlineX } from "react-icons/hi";
 
 const GraficaForm = () => {
+  const { setOpenModalGrafica } = useContext(SmartgrowContext);
+  const [selectedTimeRange, setSelectedTimeRange] = useState("ALL");
+  const [endDate, setEndDate] = useState();
   const [chartData, setChartData] = useState({
     series: [
       {
@@ -16,12 +22,10 @@ const GraficaForm = () => {
           autoScaleYaxis: true,
         },
         toolbar: {
-          style: {
-            width: "100%",
-            height: "800px",
-          },
+          show: false,
         },
       },
+      colors: ["#6A994E"],
       dataLabels: {
         enabled: false,
       },
@@ -31,7 +35,7 @@ const GraficaForm = () => {
       },
       xaxis: {
         type: "datetime",
-        min: new Date("01 Dec 2023").getTime(),
+        min: new Date("01 Mar 2021").getTime(),
         tickAmount: 6,
         labels: {
           style: {
@@ -42,17 +46,18 @@ const GraficaForm = () => {
       yaxis: {
         labels: {
           style: {
-            fontSize: "16px",
+            fontSize: "20px",
           },
         },
       },
       tooltip: {
         x: {
-          format: "dd MMM yyyy",
+          format: "dd MMM yyyy HH:mm",
         },
-      },
-      toolbar: {
-        show: false,
+
+        style: {
+          fontSize: "16px",
+        },
       },
       fill: {
         type: "gradient",
@@ -64,11 +69,14 @@ const GraficaForm = () => {
         },
       },
       title: {
-        text: "Stock Price Movement",
+        text: "Grafica de temperatura",
         align: "left",
+        style: {
+          fontSize: "24px",
+          color: "#6A994E",
+        },
       },
     },
-    selection: "one_year",
   });
 
   useEffect(() => {
@@ -82,6 +90,7 @@ const GraficaForm = () => {
           new Date(item.fecha).getTime(),
           parseFloat(item.temperatura.toFixed(2)),
         ]);
+        setEndDate(new Date(dataApi[dataApi.length - 1].fecha).getTime());
         setChartData((prevChartData) => ({
           ...prevChartData,
           series: [
@@ -89,6 +98,13 @@ const GraficaForm = () => {
               data: transformedData,
             },
           ],
+          options: {
+            ...prevChartData.options,
+            xaxis: {
+              ...prevChartData.options.xaxis,
+              min: new Date(dataApi[0].fecha).getTime(),
+            },
+          },
         }));
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -98,21 +114,53 @@ const GraficaForm = () => {
     fetchData();
   }, []);
 
+  const handleTimeRangeChange = (timeRange) => {
+    setSelectedTimeRange(timeRange);
+    console.log(endDate);
+    const filteredData = chartData.series[0].data.filter(
+      (item) => endDate - item[0] <= getTimeRangeMiliseconds(timeRange)
+    );
+
+    setChartData((prevChartData) => ({
+      ...prevChartData,
+      series: [
+        {
+          data: filteredData,
+        },
+      ],
+      options: {
+        ...prevChartData.options,
+        xaxis: {
+          ...prevChartData.options.xaxis,
+          min: endDate - getTimeRangeMiliseconds(timeRange),
+        },
+      },
+    }));
+  };
+
+  const getTimeRangeMiliseconds = (timeRange) => {
+    switch (timeRange) {
+      case "1H":
+        return 60 * 60 * 1000;
+    }
+  };
+
   return (
-    <div id="chart">
-      <div className="">
+    <div className="w-auto h-auto p-4 relative bg-white rounded-lg shadow-lg flex flex-col space-y-2 justify-center items-center">
+      <div className="w-full text-xl text-primary flex gap-6 p-2">
+        <button
+          className="p-2 rounded-lg active:bg-secondary active:text-white"
+          onClick={() => handleTimeRangeChange("1H")}
+        >
+          1H
+        </button>
+        <button>6H</button>
+        <button>1D</button>
+        <button>1S</button>
         <button>1M</button>
-        &nbsp;
-        <button>6M</button>
-        &nbsp;
-        <button>1Y</button>
-        &nbsp;
-        <button>YTD</button>
-        &nbsp;
         <button>ALL</button>
       </div>
-
-      <div id="chart-timeline" className="bg-white">
+      <div id="chart-timeline">
         <ReactApexChart
           options={chartData.options}
           series={chartData.series}
@@ -121,6 +169,12 @@ const GraficaForm = () => {
           width={1200}
         />
       </div>
+      <HiOutlineX
+        className="w-9 h-9 p-2 bg-red-500 text-white cursor-pointer rounded-full absolute -right-3 -top-5 "
+        onClick={() => {
+          setOpenModalGrafica(false);
+        }}
+      />
     </div>
   );
 };
