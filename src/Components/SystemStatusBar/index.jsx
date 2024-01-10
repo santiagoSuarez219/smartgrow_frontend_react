aab37bf0c751b5615c7bca59caf5d9d4d7693b32
santiagoSuarez219 <santiago8628@gmail.com>
2024-01-10 15:25:11 -0500

    Refactor component imports and handle navigation and modal clicks in TabBar

--- src/Components/SystemStatusBar/index.jsx
+++ src/Components/SystemStatusBar/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 
 import { HiCheck } from "react-icons/hi";
 import { HiOutlineX } from "react-icons/hi";
--- src/Components/TabBar/index.jsx
+++ src/Components/TabBar/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { HiHome } from "react-icons/hi";
 import { RiPlantFill } from "react-icons/ri";
@@ -9,8 +9,26 @@ import { SmartgrowContext } from "../../SmartgrowContext";
 
 const TabBar = () => {
   const { openModal, setOpenModal } = useContext(SmartgrowContext);
+
+  const handleNavigationClick = () => {
+    setOpenModal({
+      ...openModal,
+      actuadores: false,
+      control: false,
+    });
+  };
+
+  const handleModalClick = (modalType) => {
+    setOpenModal({
+      ...openModal,
+      actuadores: modalType === "actuadores",
+      control: modalType === "control",
+    });
+  };
+
   const activeStyle = "text-primary flex flex-col items-center";
   const deactiveStyle = "text-primary/50 flex flex-col items-center";
+
   return (
     <div className="w-full h-16 bg-white fixed left-0 bottom-0 shadow-md flex items-center justify-around z-20 lg:hidden">
       <NavLink
@@ -20,13 +38,7 @@ const TabBar = () => {
             ? activeStyle
             : deactiveStyle
         }
-        onClick={() => {
-          setOpenModal({
-            ...openModal,
-            actuadores: false,
-            control: false,
-          });
-        }}
+        onClick={handleNavigationClick}
       >
         <HiHome className="w-6 h-6" />
         <p className="text-base">Cultivo</p>
@@ -38,39 +50,21 @@ const TabBar = () => {
             ? activeStyle
             : deactiveStyle
         }
-        onClick={() => {
-          setOpenModal({
-            ...openModal,
-            actuadores: false,
-            control: false,
-          });
-        }}
+        onClick={handleNavigationClick}
       >
         <RiPlantFill className="w-6 h-6" />
         <p className="text-base">Hidroponico</p>
       </NavLink>
       <div
         className={openModal.actuadores ? activeStyle : deactiveStyle}
-        onClick={() => {
-          setOpenModal({
-            ...openModal,
-            actuadores: !openModal.actuadores,
-            control: false,
-          });
-        }}
+        onClick={() => handleModalClick("actuadores")}
       >
         <FaPowerOff className="w-6 h-6" />
         <p className="text-base">Actuadores</p>
       </div>
       <div
         className={openModal.control ? activeStyle : deactiveStyle}
-        onClick={() => {
-          setOpenModal({
-            ...openModal,
-            actuadores: false,
-            control: !openModal.control,
-          });
-        }}
+        onClick={() => handleModalClick("control")}
       >
         <GrPowerCycle className="w-6 h-6" />
         <p className="text-base">Control</p>
