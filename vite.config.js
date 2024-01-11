c19ef534ce7deb54fa169e37c23683f525ab9eb6
santiagoSuarez219 <santiago8628@gmail.com>
2024-01-11 16:07:42 -0500

    base updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://santiagoSuarez219.github.io/smartgrow_frontend_react_prod",
+});
